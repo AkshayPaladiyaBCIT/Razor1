8e3c247500dfa6e8a3a1f864930de64907d3a882
Jayshree Paladiya <jayshreeborad1@gmail.com>
2023-09-17 03:02:06 -0400

    added zodiac in privacy page

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor1.Models;
 
 namespace Razor1.Pages;
 
 public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
+    [BindProperty]
+    public int Year { get; set; }
+    public string ZodiacSign { get; set; }
+
+       
 
     public PrivacyModel(ILogger<PrivacyModel> logger)
     {
@@ -14,20 +20,30 @@ public class PrivacyModel : PageModel
 
     public void OnGet() //Get
     {
-        ViewData["Name"] = "John Smith";
+        
     }
 
     public void OnPut() //Update
     {
     }
 
-    public void OnPost() //Insert
-    {
-    }
+      public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                ZodiacSign = Utils.GetZodiac(Year);
+                return Page();
+            }
+            else
+            {
+                return Page();
+            }
+        }
 
     public void OnDelete() //Delete
     {
     }
+     
 
 }
 
